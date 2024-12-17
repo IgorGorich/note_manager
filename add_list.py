3b029f3f76bc9181defaab8bfa36e551b982af6e
IgorGorich <igo180881@gmail.com>
2024-12-17 23:41:22 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,6 @@
+username = input('Напишите своё имя ')
+city = input('Напишите название города, в котором вы живёте ')
+street = input('Напишите название улицы, на которой вы проживаете ')
+house = input('Напишите номер вашего дома ')
+home_adress = (city, street, house)
+print(username, 'вы проживаете по адресу', home_adress)
