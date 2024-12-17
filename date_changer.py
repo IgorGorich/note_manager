15f7b905dc070feaf033de9a1ed657b1391dbd54
IgorGorich <igo180881@gmail.com>
2024-12-17 22:24:13 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,8 @@
+username = 'Игорь Горич'
+title = 'Домашняя работа'
+content = 'Выполнить решение работы'
+status = 'В процессе'
+created_date = '16-12-2024'
+print(created_date[0:5])
+issue_date = '30-12-2024'
+print(issue_date[0:5])
