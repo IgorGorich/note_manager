296230cbb61f9d79d49ba7d9c444f79bb5ab6a1f
IgorGorich <igo180881@gmail.com>
2024-12-18 21:39:08 +0300

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -7,6 +7,6 @@ print('описание заметки:', content)
 status = 'В процессе'
 print('статус заметки:', status)
 created_date = '16-12-2024'
-print('16-12-2024:', created_date)
-issue_date = '16-01-2025'
-print('16-01-2025:', issue_date)
+print('Дата начала:', created_date)
+issue_date = '30-12-2024'
+print('Дата окончания:', issue_date)
