a636d2756027b45e716d3a10ef3c2dfd8880828a
IgorGorich <igo180881@gmail.com>
2025-01-27 19:33:11 +0300

    Add files via upload

--- update_note_function.py
+++ update_note_function.py
@@ -1,38 +1,6 @@
 from datetime import datetime, timedelta
 
-note = {}
-
-while True:  # Запускаем ввод имени и его проверку на пустую запись
-    note['username'] = input('Напишите имя пользователя: ')
-    if note['username'] == "" or note['username'] == " ":
-        print('Вы не написали имя, повторите!')
-    else:
-        break
-note['title'] = input('Введите заголовок заметки: ')
-note['content'] = input('Введите описание заметки: ')
-print('Выберите статус заметки, нажав соответствующую цифру:', '1. Новая', '2. В процессе', '3. Выполнено',
-      sep='\n')
-while True:  # Запускаем цикл по определению статуса заметки
-    status_num = input()
-    if status_num == '1':
-        note['status'] = 'Новая'
-        break
-    elif status_num == '2':
-        note['status'] = 'В процессе'
-        break
-    elif status_num == '3':
-        note['status'] = 'Выполнено'
-        break
-    else:  # Если значение отсутствует из предложенных, сообщаем об ошибке и просим повторить
-        print('Вы неправильно выбрали статус, повторите')
-created_date_str = datetime.today()
-note['created_date'] = created_date_str.strftime("%d-%m-%Y")  # Дата создания = сегодняшняя дата
-issue_date_str = created_date_str + timedelta(days=7)
-note['issue_date'] = issue_date_str.strftime("%d-%m-%Y")  # Дедлайн через неделю от сегодняшней даты
-print('Ваша заметка содержит:')
-
-for i in note:
-    print(*note[i])
+from create_note_function import create_note, note
 
 
 def update_note():
